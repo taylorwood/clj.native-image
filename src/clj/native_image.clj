874c798d7e4bc97c8864de8a6d4410abea42a09d
Taylor Wood <taylorwood@me.com>
2019-02-05 21:23:36 -0600

    Stream native-image stdout/stderr to stdout

--- src/clj/native_image.clj
+++ src/clj/native_image.clj
@@ -1,10 +1,10 @@
 (ns clj.native-image
+  "Builds GraalVM native images from deps.edn projects."
   (:require [clojure.java.io :as io]
-            [clojure.java.shell :refer [sh]]
             [clojure.string :as cs]
             [clojure.tools.deps.alpha :as deps]
             [clojure.tools.deps.alpha.reader :as deps.reader])
-  (:import (java.io File)))
+  (:import (java.io File BufferedReader)))
 
 (defn deps->classpath
   "Returns the classpath according to deps.edn, adds *compile-path*."
@@ -19,58 +19,69 @@
       (concat ["deps.edn"])
       (deps.reader/read-deps)))
 
+(defn sh
+  "Launches a process with optional args, returning exit code.
+  Prints stdout & stderr."
+  [bin & args]
+  (let [arg-array ^"[Ljava.lang.String;" (into-array String (cons bin args))
+        process (-> (ProcessBuilder. arg-array)
+                    (.redirectErrorStream true) ;; TODO stream stderr to stderr
+                    (.start))]
+    (with-open [out (io/reader (.getInputStream process))]
+      (loop []
+        (when-let [line (.readLine ^BufferedReader out)]
+          (println line)
+          (recur))))
+    (.waitFor process)))
+
 (defn exec-native-image
   "Executes native-image (bin) with opts, specifying a classpath,
    main/entrypoint class, and destination path."
   [bin opts cp main]
-  (let [all-args (cond-> []
+  (let [cli-args (cond-> []
                    (seq opts) (into opts)
-                   cp (into ["-cp" cp])
-                   main (conj main))]
-    (apply sh bin all-args)))
-
-(defn build-native-image [cp main nat-img-path opts]
-  (println (format "Building native image '%s' with classpath '%s'" main cp))
-  (let [{:keys [exit out err]}
-        (exec-native-image nat-img-path (conj opts "--no-server") cp main)]
-    ;; TODO would be nice to stream output here
-    (some-> err not-empty println)
-    (some-> out not-empty println)
-    exit))
+                   cp         (into ["-cp" cp])
+                   main       (conj main)
+                   :always    (conj "--no-server"))]
+    (apply sh bin cli-args)))
 
 (defn prep-compile-path []
   (doseq [file (-> (io/file *compile-path*) (file-seq) (rest) (reverse))]
     (io/delete-file file))
   (.mkdir (io/file *compile-path*)))
 
-(defn native-image-path []
+(defn native-image-bin-path []
   (-> (io/file (System/getenv "GRAALVM_HOME") "bin/native-image")
       (.getAbsolutePath)))
 
 (defn- munge-class-name [class-name]
   (cs/replace class-name "-" "_"))
 
-(defn -main [main & opts]
+(defn build [main-ns opts]
   (let [[nat-img-path & nat-img-opts]
         (if (some-> (first opts) (io/file) (.exists)) ;; check first arg is file path
           opts
-          (cons (native-image-path) opts))]
-    (when-not (string? main)
+          (cons (native-image-bin-path) opts))]
+    (when-not (string? main-ns)
       (binding [*out* *err*] (println "Main namespace required e.g. \"script\" if main file is ./script.clj"))
       (System/exit 1))
 
-    (println "Loading" main)
-    (load (-> main
+    (println "Loading" main-ns)
+    (load (-> main-ns
               (cs/replace "." File/separator)
               (munge-class-name)))
 
-    (println "Compiling" main)
+    (println "Compiling" main-ns)
     (prep-compile-path)
-    (compile (symbol main))
+    (compile (symbol main-ns))
 
-    (System/exit
-      (build-native-image
-       (deps->classpath (merged-deps))
-       (munge-class-name main)
-       nat-img-path
-       nat-img-opts))))
+    (let [classpath (deps->classpath (merged-deps))
+          class-name (munge-class-name main-ns)]
+      (println (format "Building native image '%s' with classpath '%s'" class-name classpath))
+      (System/exit (exec-native-image nat-img-path nat-img-opts classpath class-name)))))
+
+(defn -main [main-ns & args]
+  (try
+    (build main-ns args)
+    (finally
+      (shutdown-agents))))
